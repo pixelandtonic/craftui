21b4dd084437e6f97ef94d4c42523c146a33c44c
Benjamin David <ben@dukt.net>
2018-12-04 15:53:19 +0100

    Storybook

---
+++ src/components/Btn.vue
@@ -0,0 +1,29 @@
+<template>
+  <button :class="'btn ' + (type ? type : 'default')" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+    export default {
+        props: ['type'],
+    }
+</script>
+
+<style lang="scss">
+button {
+  @apply .px-4 .py-2 .rounded .bg-grey-lighter;
+
+  &.primary {
+    @apply .bg-blue .text-white;
+  }
+
+  &.warning {
+    @apply .bg-orange .text-white;
+  }
+
+  &.danger {
+    @apply .bg-red .text-white;
+  }
+}
+</style>
