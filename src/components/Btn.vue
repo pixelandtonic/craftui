bcb82e0a419a23de1ae6c125bf9731e100802366
Benjamin David <ben@dukt.net>
2018-12-06 18:15:18 +0100

    Added `large` and `block` props

--- src/components/Btn.vue
+++ src/components/Btn.vue
@@ -1,18 +1,34 @@
 <template>
-  <button :class="'btn ' + (type ? type : 'default')" @click="$emit('click')">
+  <button :class="'c-btn '
+                      + (type ? type : 'default')
+                      + (typeof large !== 'undefined' ? ' large' : '')
+                      + (typeof block !== 'undefined' ? ' block' : '')"
+                      @click="$emit('click')">
     <slot></slot>
   </button>
 </template>
 
 <script>
     export default {
-        props: ['type'],
+        props: ['type', 'large', 'block'],
     }
 </script>
 
-<style lang="scss">
-button {
+<style lang="scss" scoped>
+.c-btn {
   @apply .px-4 .py-2 .rounded .bg-grey-lighter;
+
+  &.block {
+    @apply .w-full;
+  }
+
+  &.large {
+    @apply .text-lg .py-3;
+  }
+
+  &.large {
+    @apply .text-lg .py-3;
+  }
 
   &.primary {
     @apply .bg-blue .text-white;
