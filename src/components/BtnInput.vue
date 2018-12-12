a3bc7ad3f146d60912e05796add8663ef3f5bb7e
Benjamin David <ben@dukt.net>
2018-12-12 10:29:16 +0100

    Added BtnInput

---
+++ src/components/BtnInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="submit" :class="cssClass"
+           :disabled="disabled"
+           :value="value"
+           @click="$emit('click')"
+    />
+</template>
+
+<script>
+    import Btn from './Btn'
+
+    export default {
+
+        props: ['value'],
+
+        extends: Btn,
+    }
+</script>
