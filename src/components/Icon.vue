7a0ab63467085d6dba2997cfae8a8acd2d366065
Benjamin David <ben@dukt.net>
2019-01-22 15:31:30 +0100

    Removed wrapping div

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,7 +1,5 @@
 <template>
-    <div>
-        <font-awesome-icon :icon="icon" :class="cssClass"></font-awesome-icon>
-    </div>
+    <font-awesome-icon :icon="icon" :class="cssClass"></font-awesome-icon>
 </template>
 
 <script>
