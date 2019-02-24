7fd49832b8f9695f386282c2888ee62c0f4263ce
Benjamin David <ben@dukt.net>
2019-02-24 11:38:23 +0100

    Added input stories

---
+++ docs/src/stories/components-inputs.stories.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue'
+
+storiesOf('Components|Inputs/Lightswitch', module)
+    .add('Checked', () => ({
+        template: '<lightswitch-input :checked="true"></lightswitch-input>',
+    }))
+    .add('Unchecked', () => ({
+        template: '<lightswitch-input :checked="false"></lightswitch-input>',
+    }));
+
+storiesOf('Components|Inputs/Password', module)
+    .add('Default', () => ({
+        template: '<password-input></password-input>',
+    }));
+
+storiesOf('Components|Inputs/Select', module)
+    .add('Default', () => ({
+        template: '<select-input :options="[{label:\'One\', value:1}, {label:\'Two\', value:2}, {label:\'Three\', value:3}]"></select-input>',
+    }));
+
+
+storiesOf('Components|Inputs/Textarea', module)
+    .add('Default', () => ({
+        template: '<textarea-input id="textarea-default" label="Textarea"></textarea-input>',
+    }));
+
+storiesOf('Components|Inputs/Text', module)
+    .add('Default', () => ({
+        template: '<text-input></text-input>',
+    }));
+
+storiesOf('Components|Inputs/URL', module)
+    .add('Default', () => ({
+        template: '<url-input></url-input>',
+    }));
