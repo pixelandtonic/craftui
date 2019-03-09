7e56a5805e2c02bf2b243cee862a03e0406ca19b
Benjamin David <ben@dukt.net>
2019-03-09 11:24:15 +0100

    Fixed story source

--- docs/config/webpack.config.js
+++ docs/config/webpack.config.js
@@ -9,5 +9,12 @@ module.exports = (baseConfig, env, defaultConfig) => {
     });
     defaultConfig.resolve.extensions.push(".scss");
 
+    // Storysource
+    defaultConfig.module.rules.push({
+        test: /\.stories\.jsx?$/,
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        enforce: 'pre',
+    });
+
     return defaultConfig;
 };
