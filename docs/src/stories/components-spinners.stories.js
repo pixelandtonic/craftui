66175b88f2faf3064901f22307097e348bd852fd
Benjamin David <ben@dukt.net>
2019-03-10 21:09:32 +0100

    Added notes to the `Spinner` component story

--- docs/src/stories/components-spinners.stories.js
+++ docs/src/stories/components-spinners.stories.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue'
+import SpinnerNotesMD from '../notes/Spinner.md'
 
 storiesOf('Components|Spinners', module)
     .add('base', () => ({
         template: '<spinner></spinner>',
-    }))
+    }), { notes: { markdown: SpinnerNotesMD } })
     .add('lg', () => ({
         template: '<spinner class="lg"></spinner>',
-    }))
+    }), { notes: { markdown: SpinnerNotesMD } })
