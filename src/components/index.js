e31c4a716160d3b4d159ab65c7695a3405e14642
Benjamin David <ben@dukt.net>
2019-03-13 12:42:21 +0100

    Clean up

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,25 @@
 import Btn from './Btn.vue';
 import Checkbox from './Checkbox.vue';
 import CheckboxSet from './CheckboxSet.vue';
+import Dropdown from './Dropdown.vue';
 import Field from './Field.vue';
 import Icon from './Icon.vue';
 import Lightswitch from './Lightswitch.vue';
-import Dropdown from './Dropdown.vue';
 import Radio from './Radio.vue';
 import Spinner from './Spinner.vue';
 import Textbox from './Textbox.vue';
 
 const CraftComponents = {
-  Btn,
-  Checkbox,
-  CheckboxSet,
-  Field,
-  Icon,
-  Lightswitch,
-  Dropdown,
-  Radio,
-  Spinner,
-  Textbox,
+    Btn,
+    Checkbox,
+    CheckboxSet,
+    Dropdown,
+    Field,
+    Icon,
+    Lightswitch,
+    Radio,
+    Spinner,
+    Textbox,
 };
 
 export default CraftComponents;
