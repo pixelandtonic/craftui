061da7c8776ff9ceb6a38e826b9df3433bfdff96
Benjamin David <ben@dukt.net>
2019-03-15 17:57:14 +0100

    Removed unused variable

--- src/plugins/icons.js
+++ src/plugins/icons.js
@@ -1,7 +1,7 @@
 import regularSvg from '../sprites/solid.svg'
 
 export default {
-    install (Vue) {
+    install () {
         if(typeof document !== 'undefined'){
             const iconsWrapper = document.createElement('div')
             iconsWrapper.style.display = 'none'
