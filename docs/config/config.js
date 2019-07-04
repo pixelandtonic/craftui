617beae0480fd8b56a422cfbe59458683d92a70b
Benjamin David <ben@dukt.net>
2019-07-04 13:21:11 +0200

    Clean up code

--- docs/config/config.js
+++ docs/config/config.js
@@ -1,6 +1,7 @@
 // Craft UI
 import Vue from 'vue'
 import CraftUi from '../../dist/craftui.es'
+
 Vue.use(CraftUi)
 import '../../dist/craftui.css'
 
@@ -11,8 +12,8 @@ import '../src/sass/styles.scss'
 // Stories
 
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure, addDecorator } from '@storybook/vue'
-import { setOptions } from '@storybook/addon-options'
+import {configure, addDecorator} from '@storybook/vue'
+import {setOptions} from '@storybook/addon-options'
 import {withNotes} from '@storybook/addon-notes'
 import {withKnobs} from '@storybook/addon-knobs'
 
@@ -21,19 +22,19 @@ addDecorator(withKnobs)
 addDecorator(withNotes)
 
 setOptions({
-  name: 'Craft UI',
-  url: 'https://github.com/pixelandtonic/craftui',
-  showStoriesPanel: true,
-  hierarchySeparator: /\/|\./, // matches a . or /
-  hierarchyRootSeparator: /\|/, //matches a |
+    name: 'Craft UI',
+    url: 'https://github.com/pixelandtonic/craftui',
+    showStoriesPanel: true,
+    hierarchySeparator: /\/|\./, // matches a . or /
+    hierarchyRootSeparator: /\|/, //matches a |
 })
 
 
 function loadStories() {
-  require('../src/stories')
+    require('../src/stories')
 
-  const req = require.context('../src/stories', true, /.stories.js$/)
-  req.keys().forEach(filename => req(filename))
+    const req = require.context('../src/stories', true, /.stories.js$/)
+    req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
