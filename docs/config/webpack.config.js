1d08405227050523595e7f81229924501e6d393e
Benjamin David <ben@dukt.net>
2019-07-04 13:16:57 +0200

    Updated Storybook JS to 5.1.9

--- docs/config/webpack.config.js
+++ docs/config/webpack.config.js
@@ -1,20 +1,20 @@
 const path = require("path");
 
-module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = ({config, mode}) => {
     // Add support for .scss files.
-    defaultConfig.module.rules.push({
+    config.module.rules.push({
         test: /\.(scss)$/,
         include: path.resolve(__dirname, "../src/sass/"),
         loaders: [require.resolve("style-loader"), require.resolve("css-loader"), require.resolve("sass-loader")],
     });
-    defaultConfig.resolve.extensions.push(".scss");
+    config.resolve.extensions.push(".scss");
 
     // Storysource
-    defaultConfig.module.rules.push({
+    config.module.rules.push({
         test: /\.stories\.jsx?$/,
         loaders: [require.resolve('@storybook/addon-storysource/loader')],
         enforce: 'pre',
     });
 
-    return defaultConfig;
+    return config;
 };
