fd39ca8e6024a90a3342e2869929f7ca9c31c335
Benjamin David <ben@dukt.net>
2019-07-23 19:59:30 +0200

    Tailwind component example

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,48 @@ module.exports = {
             colors: semanticColors
         }
     },
+
+    plugins: [
+        // Add components
+        function({ addComponents, theme }) {
+            const buttons = {
+                '.xbtn': {
+                    backgroundColor: theme('colors.secondary-background'),
+                    padding: theme('spacing.4'),
+                    borderRadius: theme('borderRadius.lg'),
+                    fontWeight: theme('fontWeight.extraBold'),
+                },
+                '.xbtn > .xicon': {
+                    width: 20,
+                    height: 20,
+                    background: 'red',
+                    display: 'inline-block  ',
+                },
+                '.xbtn-primary': {
+                    backgroundColor: theme('colors.interactive-primary'),
+                    color: theme('colors.interactive-primary-text'),
+                    '&:hover': {
+                        backgroundColor: theme('colors.interactive-primary-hover'),
+                        cursor: 'pointer'
+                    },
+                    '&:active': {
+                        backgroundColor: theme('colors.interactive-primary-active')
+                    },
+                },
+                '.xbtn-danger': {
+                    backgroundColor: theme('colors.interactive-danger'),
+                    color: theme('colors.interactive-danger-text'),
+                    '&:hover': {
+                        backgroundColor: theme('colors.interactive-danger-hover'),
+                        cursor: 'pointer'
+                    },
+                    '&:active': {
+                        backgroundColor: theme('colors.interactive-danger-active')
+                    },
+                },
+            }
+
+            addComponents(buttons)
+        }
+    ]
 }
