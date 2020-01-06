ba4396eb2e0452dabf7e5aecf90699497a3cb7a4
Benjamin David <ben@dukt.net>
2020-01-06 19:01:17 +0100

    Updated Storybook JS to `5.2.8`

--- docs/config/webpack.config.js
+++ docs/config/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = ({config}) => {
     // Storysource
     config.module.rules.push({
         test: /\.stories\.jsx?$/,
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        loaders: [require.resolve('@storybook/source-loader')],
         enforce: 'pre',
     });
 
