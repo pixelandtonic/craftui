41e710e9f1e2c9387dd71cdfc15c789176741be6
Benjamin David <ben@dukt.net>
2020-07-14 02:01:36 +0200

    Added alert examples

--- docs/src/stories/colors.stories.js
+++ docs/src/stories/colors.stories.js
@@ -1,5 +1,6 @@
 import SemanticColorsApp from './SemanticColors.vue'
 import ColorExamplesApp from './ColorExamples.vue'
+import AlertsApp from './Alerts.vue'
 
 export default {
     title: 'Colors',
@@ -11,4 +12,8 @@ export const SemanticColors = () => ({
 
 export const Examples = () => ({
     render: h => h(ColorExamplesApp),
+})
+
+export const Alerts = () => ({
+    render: h => h(AlertsApp),
 })
