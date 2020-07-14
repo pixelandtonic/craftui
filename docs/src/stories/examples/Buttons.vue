2404549a54952502bcba20479095e7163bee6430
Benjamin David <ben@dukt.net>
2020-07-14 18:18:28 +0200

    Added disabled and icon buttons

--- docs/src/stories/examples/Buttons.vue
+++ docs/src/stories/examples/Buttons.vue
@@ -8,6 +8,19 @@
                 {{buttonType}}
             </btn>
         </div>
+        <h2 :class="`text-2xl font-medium mt-6 mb-2`">Disabled</h2>
+        <div class="border p-12 max-w-3xl mx-auto space-y-4 flex flex-col items-center justify-start sm:space-y-0 sm:flex-row sm:items-end sm:justify-around">
+            <btn v-for="buttonType in buttonTypes" :kind="buttonType" :disabled="true">
+                {{buttonType}}
+            </btn>
+        </div>
+
+        <h2 :class="`text-2xl font-medium mt-6 mb-2`">Icon</h2>
+        <div class="border p-12 max-w-3xl mx-auto space-y-4 flex flex-col items-center justify-start sm:space-y-0 sm:flex-row sm:items-end sm:justify-around">
+            <btn v-for="buttonType in buttonTypes" :kind="buttonType" icon="plus">
+                {{buttonType}}
+            </btn>
+        </div>
 
         <h2 :class="`text-2xl font-medium mt-6 mb-2`">Loading</h2>
         <div class="border p-12 max-w-3xl mx-auto space-y-4 flex flex-col items-center justify-start sm:space-y-0 sm:flex-row sm:items-end sm:justify-around">
