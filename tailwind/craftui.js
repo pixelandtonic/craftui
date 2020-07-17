77dad975a1dd1b745c518611a379d8699c5e7871
Benjamin David <ben@dukt.net>
2020-07-17 03:47:40 +0200

    Clean up

--- tailwind/craftui.js
+++ tailwind/craftui.js
@@ -93,11 +93,11 @@ module.exports = craftui.withOptions(
                 },
             })
 
+
             // Gradients
 
             const gradients = theme('gradients', {})
             const gradientVariants = variants('gradients', [])
-
             const utilities = _.map(gradients, ([start, end], name) => ({
                 [`.bg-gradient-${e(name)}`]: {
                     backgroundImage: `linear-gradient(to bottom, ${start}, ${end})`
